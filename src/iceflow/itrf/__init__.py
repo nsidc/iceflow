2bc2b6be8cec3fe91edf16d14e1fa4e5ca2dac03
Trey Stafford <trey.stafford@colorado.edu>
2024-08-13 16:41:19 -0600

    TODO

--- src/iceflow/itrf/__init__.py
+++ src/iceflow/itrf/__init__.py
@@ -4,6 +4,10 @@ from typing import Literal, get_args
 
 # ITRF strings recognized by proj, which is used in the ITRF transformation
 # code.
+# TODO: pyproj does recognize e.g., `ITRF1993` to be an alias of
+# `ITRF93`. Instead of being a hard-coded list, we could create a custom
+# validator for pandera that ensures the value is e.g., `ITRF\d{4}`, and then
+# the itrf transformation code could handle unrecognized ITRFs.
 ITRF = Literal[
     "ITRF93",
     "ITRF94",
