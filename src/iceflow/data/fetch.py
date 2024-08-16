edd321a6f90bcfeaf06d805eec0c701905d79672
Trey Stafford <trey.stafford@colorado.edu>
2024-08-16 14:14:58 -0600

    Docstring

--- src/iceflow/data/fetch.py
+++ src/iceflow/data/fetch.py
@@ -17,6 +17,13 @@ def search_and_download(
     temporal: tuple[str, str],
     output_dir: Path,
 ) -> list[Path]:
+    """Search and download data.
+
+    Wraps EDL auth and CMR search using `earthaccess`.
+
+    Data matching the given parameters are downloaded to a subfolder of the
+    given `output_dir` named after th e`short_name`.
+    """
     earthaccess.login()
 
     results = earthaccess.search_data(
