ad67cae6d64364bf0b034a86ed5f759f2ae123fa
Trey Stafford <trey.stafford@colorado.edu>
2024-08-20 08:38:07 -0600

    Tweak assertions in itrf check unit test

--- tests/unit/test_itrf.py
+++ tests/unit/test_itrf.py
@@ -4,9 +4,10 @@ from iceflow.itrf import check_itrf
 
 
 def test_check_itrf():
-    assert check_itrf("Not an ITRF string") is False
-    assert check_itrf("ITRF") is False
+    # These should return False
+    assert not check_itrf("Not an ITRF string")
+    assert not check_itrf("ITRF")
 
-    # These should pass.
-    assert check_itrf("ITRF2008") is True
-    assert check_itrf("ITRF88") is True
+    # These should return True
+    assert check_itrf("ITRF2008")
+    assert check_itrf("ITRF88")
