90746c52348a8880f84e4ac055aa774a7bd36dce
Trey Stafford <trey.stafford@colorado.edu>
2024-10-16 16:25:59 -0600

    Update note about GLAH06 data variables

--- src/nsidc/iceflow/data/models.py
+++ src/nsidc/iceflow/data/models.py
@@ -101,9 +101,11 @@ class ILVIS2Schema(CommonDataColumnsSchema):
 
 
 class GLAH06Schema(CommonDataColumnsSchema):
-    # Note: all of these variables are extracted from the "Data_40HZ" group. We
-    # may want to support accessing data from the "Data_1HZ" group in the
-    # future.
+    # Note: all of these variables are extracted from the "Data_40HZ"
+    # group. There is also a "Data_1HZ" group, which contains similar variables,
+    # but does not include the elevation data and appears to be a downsampled
+    # version of the "Data_40HZ" group. See
+    # https://github.com/nsidc/iceflow/issues/43.
     i_rec_ndx: Series[int] = pa.Field(coerce=True)
     i_shot_count: Series[int] = pa.Field(coerce=True)
     d_lat: Series[float]
